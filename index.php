4489080675386b314e9f989a83456070b06824a6
André Moura <andre.benedicto@etec.sp.gov.br>
2020-09-19 16:29:15 -0300

    Iniciando a marcação com HTML5

---
+++ index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto 01</title>
+    <meta name="description" content="Descrição do meu website">
+    <meta name="keywords" content="palavras-chave, do, meu, site">
+</head>
+<body>
+
+    <header>
+        <div class="logo">Logomarca</div>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Serviços</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section class="banner-principal">
+        <div class="container">
+            <form action="">
+                <h2>Qual é o seu melhor e-mail?</h2>
+                <input type="text" name="email" required>
+                <button name="acao">Cadastrar</button>
+            </form>
+        </div>
+    </section>
+
+    <section class="descricao-autor">
+        <div class="container">
+            <div class="w-50">
+                <h2>André Moura</h2>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum ullam velit maiores? Minus, dolore facere iste eum, nihil ex consequuntur debitis vitae commodi nam aut quisquam veniam cupiditate facilis repellendus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quisquam debitis nobis sunt voluptate reiciendis velit beatae voluptatem dicta quia impedit, unde excepturi accusantium aperiam dolorem ducimus! Earum, iure laudantium!
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi provident quidem officia in laudantium consequuntur exercitationem, aliquam quos laboriosam quia illum quisquam pariatur soluta neque ex facilis. Harum, ut culpa? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum corporis tempore quidem qui non ipsam, assumenda saepe, architecto ipsa voluptatum sequi accusamus dignissimos dolorem eaque autem magnam dolores laboriosam sint!
+                </p>
+            </div>
+        </div>
+
+        <div class="w-50">
+            <img src="./images/André-Moura.jpg" alt="">
+        </div>
+    </section>
+
+    <section class="especialidades">
+        <div class="container">
+        <h2 class="title">Especialidades</h2>
+            <div class="box-especialidade">
+                <h3>Icone1</h3>
+                <h3>CSS3</h3>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio excepturi aspernatur doloribus recusandae, a dolorum qui natus eos voluptate sunt voluptatibus reiciendis quisquam velit doloremque modi eum consectetur commodi! Veritatis?
+                </p>
+            </div>
+
+            <div class="box-especialidade">
+                <h3>Icone2</h3>
+                <h3>HTML5</h3>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio excepturi aspernatur doloribus recusandae, a dolorum qui natus eos voluptate sunt voluptatibus reiciendis quisquam velit doloremque modi eum consectetur commodi! Veritatis?
+                </p>
+            </div>
+
+            <div class="box-especialidade">
+                <h3>Icone3</h3>
+                <h3>JavaScript</h3>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio excepturi aspernatur doloribus recusandae, a dolorum qui natus eos voluptate sunt voluptatibus reiciendis quisquam velit doloremque modi eum consectetur commodi! Veritatis?
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <section class="extras">
+        <div class="container">
+            <div class="w-50">
+                <h2 class="title">Depoimentos</h2>
+                <div class="depoimento-single">
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum facilis dolorum voluptatem consequuntur deleniti neque saepe labore, soluta numquam! Nostrum ullam porro iusto iure aut obcaecati? Doloribus impedit atque commodi!
+                    </p>
+                    <p class="nome-autor">Lorem Inpsum</p>
+                </div>
+
+                <div class="depoimento-single">
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum facilis dolorum voluptatem consequuntur deleniti neque saepe labore, soluta numquam! Nostrum ullam porro iusto iure aut obcaecati? Doloribus impedit atque commodi!
+                    </p>
+                    <p class="nome-autor">Lorem Inpsum</p>
+                </div>
+
+                <div class="depoimento-single">
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum facilis dolorum voluptatem consequuntur deleniti neque saepe labore, soluta numquam! Nostrum ullam porro iusto iure aut obcaecati? Doloribus impedit atque commodi!
+                    </p>
+                    <p class="nome-autor">Lorem Inpsum</p>
+                </div>
+            </div>
+
+            <div class="w-50">
+                <h2 class="title">Serviços</h2>
+                <div class="servicos">
+                    <ul>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore architecto amet, necessitatibus, animi nihil qui, doloremque quaerat impedit eveniet molestias harum et! Accusamus soluta iure quibusdam provident, esse ipsum asperiores.
+                        </li>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore architecto amet, necessitatibus, animi nihil qui, doloremque quaerat impedit eveniet molestias harum et! Accusamus soluta iure quibusdam provident, esse ipsum asperiores.
+                        </li>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore architecto amet, necessitatibus, animi nihil qui, doloremque quaerat impedit eveniet molestias harum et! Accusamus soluta iure quibusdam provident, esse ipsum asperiores.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="container">
+            <p>Todos os direitos reservados</p>
+        </div>
+    </footer>
+    
+</body>
+</html>
